4b8fa554c7ef7face223c91e198b8c2bea1aac01
Rumaisa Abdulhai <>
2018-12-15 14:48:50 -0500

    creates sqlite table

--- app/src/main/java/com/example/rumaisaabdulhai/duaapp/MainActivity.java
+++ app/src/main/java/com/example/rumaisaabdulhai/duaapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rumaisaabdulhai.duaapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,6 +16,10 @@ import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -40,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             DatabaseHelper databaseHelper = new DatabaseHelper(this.getApplicationContext());
             databaseHelper.createDataBase();
             SQLiteDatabase db = databaseHelper.getReadableDatabase();
+            populateDatabase(this.getApplicationContext(), db);
 
             Cursor res = db.rawQuery("select name, category from DUAS", null);
             while (res.moveToNext()) {
@@ -72,7 +78,46 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
 
+    private void populateDatabase(Context context, SQLiteDatabase database) {
+// load data
+        AssetManager assetManager = context.getAssets();
+        try {
+            String dropTableSql = "drop table if exists duas";
+            database.execSQL(dropTableSql);
+            String createTableSql = "create table if not exists duas(category varchar(255) not null, " +
+                    "name varchar(255) not null, " +
+                    "arabic text not null, " +
+                    "transliteration text, " +
+                    "translation text, " +
+                    "reference text)";
+            database.execSQL(createTableSql);
+            String deleteTableSql = "delete from duas";
+            database.execSQL(deleteTableSql);
+
+            InputStream inputStream = assetManager.open("duas.csv");
+            InputStreamReader streamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            String line;
+            String[] values;
+            String insertSql = "insert into duas(category, name, arabic, transliteration, translation, reference)" +
+                    "values(?, ?, ?, ?, ?, ?)";
+            while ((line = bufferedReader.readLine()) != null) {
+                values = line.split("\\|");
+                List<String> list = new ArrayList<>();
+                list.add(values[0]);
+                list.add(values[1]);
+                list.add(values[3]);
+                list.add(values[4]);
+                list.add(values[5]);
+                list.add(values[6]);
+                database.execSQL(insertSql, list.toArray());
+            }
+        } catch (IOException e) {
+            Log.e("TBCAE", "Failed to open data input file");
+            e.printStackTrace();
+        }
     }
 
 }
